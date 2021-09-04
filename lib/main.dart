3d242d9e0c330b7e8f1903cbf404184b6575ae83
Gayan Kalinga <bg15407@gmail.com>
2021-09-04 19:25:19 +0530

    dice app

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DiceApp(),
+      ),
+    ),
+  );
+}
+
+class DiceApp extends StatefulWidget {
+  const DiceApp({Key? key}) : super(key: key);
+
+  @override
+  _DiceAppState createState() => _DiceAppState();
+}
+
+class _DiceAppState extends State<DiceApp> {
+  int leftDiceNumber = 3;
+  int rightDiceNumber = 2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                });
+                print('Button one is pressed');
+              },
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+              onPressed: () {
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
+                print('Button two is pressed');
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
